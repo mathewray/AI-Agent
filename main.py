53e0e02d7b78abc749a114a80489de003221e1dc
Ray Mathew <ray20056@gmail.com>
2025-05-09 18:11:59 -0500

    first

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from tools import search_tool, wiki_tool,save_tool
-import os
+#import os
 
 # Load environment variables from .env file
 load_dotenv()
-print("Loaded API Key:", os.getenv("OPENAI_API_KEY"))
+
 
 class ResearchResponse(BaseModel):
     topic: str
